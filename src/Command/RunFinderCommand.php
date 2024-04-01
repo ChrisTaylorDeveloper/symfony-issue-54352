afd91ea388546a713950b7ce24a1129a46365501
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-04-01 18:36:39 +0200

    Establish working point in Docker on Linux.

---
+++ src/Command/RunFinderCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Symfony\Component\Finder\Finder;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:run-finder',
+    description: 'Add a short description for your command',
+)]
+class RunFinderCommand extends Command
+{
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $connection = ssh2_connect($_ENV['SSH_HOST'], 22);
+        ssh2_auth_password($connection, $_ENV['SSH_USER'], $_ENV['SSH_PASS']);
+        $sftp = ssh2_sftp($connection);
+
+        $finder = new Finder();
+        $finder->in('ssh2.sftp://' . intval($sftp) . $_ENV['SSH_HOST_PATH'])->files();
+
+        if ($finder->count() > 0) {
+            foreach ($finder as $file) {
+                echo $file->getMTime() . PHP_EOL;
+            }
+        }
+
+        return Command::SUCCESS;
+    }
+}
